b86803f1cb624ae8340fd4fd9f4e36ac8e4a1ac1
Erik Dalén <erik.gustav.dalen@gmail.com>
2018-11-08 11:57:02 +0100

    Switch to serverless-webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+const entries = {};
+
+Object.keys(slsw.lib.entries).forEach(
+  key => (entries[key] = ['./source-map-install.js', slsw.lib.entries[key]]),
+);
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+  // Workaround for ws module trying to require devDependencies
+  externals: ['utf-8-validate', 'bufferutil'],
+};
